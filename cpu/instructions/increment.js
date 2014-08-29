81960f4ed56ecddb2ab9c15134ee481d559e9a81
Pedro Henrique <pedrohenriquerls@gmail.com>
2014-08-29 15:18:29 -0300

    new instructions increment and jump

---
+++ cpu/instructions/increment.js
@@ -0,0 +1,185 @@
+/*package edu.fit.cs.sno.snes.cpu.instructions;
+
+import edu.fit.cs.sno.snes.common.Instruction;
+import edu.fit.cs.sno.snes.cpu.AddressingMode;
+import edu.fit.cs.sno.snes.cpu.CPU;
+
+/**
+ * Increment CPU instructions
+ */
+
+	/**
+	 * Increment Accumulator
+	 * 0x1A
+	 */ 
+
+function _incAccumulator() {
+    this.name = "Increment Accumulator"
+    this.argCount = 0;
+    this.size = Size.MEMORY_A;
+    this.addrMode = AddressingMode.ACCUMULATOR;
+    this.mnemonic = 'INC'
+}
+_incAccumulator.prototype.run = function(args){
+    CPU.a.add(1);
+			
+		CPU.status.setNegative(CPU.a.isNegative());
+		CPU.status.setZero(CPU.a.getValue() == 0);
+		
+		var cycles = 2;
+    return cycles;
+}
+
+	/**
+	 * Increment Direct Page
+	 * 0xE6
+	 */ 
+function _incDirectPage() {
+    this.name = "Increment Direct Page"
+    this.argCount = 0;
+    this.size = Size.MEMORY_A;
+    this.addrMode = AddressingMode.DIRECT_PAGE;
+    this.mnemonic = 'INC'
+}
+_incDirectPage.prototype.run = function(args){
+  CPU.loadDataRegister(this.addrMode, this.size.getRealSize(), args);
+	CPU.dataReg.add(1);
+	CPU.saveDataReg();
+	
+	CPU.status.setNegative(CPU.dataReg.isNegative());
+	CPU.status.setZero(CPU.dataReg.getValue() == 0);
+	
+	var cycles = 5;
+	if ((CPU.dp.getValue() & 0xFF) != 0)
+		cycles++;
+	if(!CPU.status.isMemoryAccess())
+		cycles += 2;
+	return cycles;
+}
+	
+	/**
+	 * Increment Absolute
+	 * 0xEE
+	 */
+function _incAbsolute() {
+    this.name = "Increment Absolute"
+    this.argCount = 0;
+    this.size = Size.MEMORY_A;
+    this.addrMode = AddressingMode.ABSOLUTE;
+    this.mnemonic = 'INC'
+}
+_incAbsolute.prototype.run = function(args){
+  CPU.loadDataRegister(this.addrMode, this.size.getRealSize(), args);
+	CPU.dataReg.add(1);
+	CPU.saveDataReg();
+	
+	CPU.status.setNegative(CPU.dataReg.isNegative());
+	CPU.status.setZero(CPU.dataReg.getValue() == 0);
+	
+	int cycles = 6;
+	if(!CPU.status.isMemoryAccess())
+		cycles += 2;
+	return cycles;
+}
+	
+	/**
+	 * Increment Direct Page Indexed, X
+	 * 0xF6
+	 */
+function _incDirectPageX() {
+  this.name = "Increment Absolute"
+  this.argCount = 0;
+  this.size = Size.MEMORY_A;
+  this.addrMode = AddressingMode.DIRECT_PAGE_INDEXED_X;
+  this.mnemonic = 'INC'
+}
+_incDirectPageX.prototype.run = function(args){
+  CPU.loadDataRegister(this.addrMode, this.size.getRealSize(), args);
+	CPU.dataReg.add(1);
+	CPU.saveDataReg();
+	
+	CPU.status.setNegative(CPU.dataReg.isNegative());
+	CPU.status.setZero(CPU.dataReg.getValue() == 0);
+	
+	var cycles = 6;
+	if ((CPU.dp.getValue() & 0xFF) != 0)
+		cycles++;
+	if(!CPU.status.isMemoryAccess())
+		cycles += 2;
+	return cycles;
+}
+
+	/**
+	 * Increment Absolute Indexed, X
+	 * 0xFE
+	 */
+function _incAbsoluteX() {
+  this.name = "Increment Absolute"
+  this.argCount = 0;
+  this.size = Size.MEMORY_A;
+  this.addrMode = AddressingMode.ABSOLUTE_INDEXED_X;
+  this.mnemonic = 'INC'
+}
+_incAbsoluteX.prototype.run = function(args){
+  CPU.loadDataRegister(this.addrMode, this.size.getRealSize(), args);
+	CPU.dataReg.add(1);
+	CPU.saveDataReg();
+	
+	CPU.status.setNegative(CPU.dataReg.isNegative());
+	CPU.status.setZero(CPU.dataReg.getValue() == 0);
+	
+	var cycles = 7;
+	if(!CPU.status.isMemoryAccess())
+		cycles += 2;
+	return cycles;
+}
+
+	/**
+	 * Increment Index Register X
+	 * 0xE8
+	 */ 
+function _incX() {
+  this.name = "Increment Index Register X"
+  this.argCount = 0;
+  this.size = Size.MEMORY_A;
+  this.addrMode = AddressingMode.ABSOLUTE_INDEXED_X;
+  this.mnemonic = 'INX'
+}
+_incX.prototype.run = function(args){
+  CPU.x.add(1);
+			
+	CPU.status.setNegative(CPU.x.isNegative());
+	CPU.status.setZero(CPU.x.getValue() == 0);
+	var cycles = 2;
+	return cycles;
+}
+
+	/**
+	 * Increment Index Register Y
+	 * 0xC8
+	 */ 
+function _incX() {
+  this.name = "Increment Index Register Y"
+  this.argCount = 0;
+  this.size = Size.MEMORY_A;
+  this.addrMode = AddressingMode.IMPLIED;
+  this.mnemonic = 'INY'
+}
+_incX.prototype.run = function(args){
+  CPU.y.add(1);
+			
+	CPU.status.setNegative(CPU.y.isNegative());
+	CPU.status.setZero(CPU.y.getValue() == 0);
+	
+	var cycles = 2;
+	return cycles;
+}
+
+Increment = {
+	incAccumulator: new _incAccumulator(),
+	incDirectPage: 	new _incDirectPage(),
+	incAbsolute: 		new _incAbsolute(),
+	incAbsoluteX: 	new _incAbsoluteX(),
+	incX: 					new _incX(),
+	incY: 					new _incY(),
+}
---
+++ cpu/instructions/jump.js
@@ -0,0 +1,185 @@
+/*package edu.fit.cs.sno.snes.cpu.instructions;
+
+import edu.fit.cs.sno.snes.common.Instruction;
+import edu.fit.cs.sno.snes.common.Size;
+import edu.fit.cs.sno.snes.cpu.AddressingMode;
+import edu.fit.cs.sno.snes.cpu.CPU;
+
+public class 
+	public static final String mnemonic = "";
+	 /**
+     * Jump Absolute
+     * 0x4C
+     */ 
+function _jumpAbsolute() {
+    this.name = "Jump Absolute"
+    this.argCount = 0;
+    this.size = Size.MEMORY_A;
+    this.addrMode = AddressingMode.ABSOLUTE;
+    this.mnemonic = 'JMP'
+}
+_jumpAbsolute.prototype.run = function(args){
+    CPU.loadDataRegister(this.addrMode, this.size.getRealSize(), args);
+    CPU.pc.setValue(CPU.dataAddr);
+    var cycles = 3;
+    return cycles;
+}
+
+    /**
+     * Jump Absolute Indirect
+     * 0x6C
+     */ 
+function _jumpAbsoluteIndirect() {
+    this.name = "Jump Absolute Indirect"
+    this.argCount = 0;
+    this.size = Size.MEMORY_A;
+    this.addrMode = AddressingMode.ABSOLUTE_INDIRECT;
+    this.mnemonic = 'JMP'
+}
+_jumpAbsoluteIndirect.prototype.run = function(args){
+    CPU.loadDataRegister(this.addrMode, this.size.getRealSize(), args);
+    CPU.pc.setValue(CPU.dataAddr);
+    var cycles = 5;
+    return cycles;
+}
+    
+    /**
+     * Jump Absolute Indexed Indirect
+     * 0x7C
+     */ 
+function _jumpAbsoluteIndexedIndirect() {
+    this.name = "Jump Absolute Indexed Indirect"
+    this.argCount = 0;
+    this.size = Size.MEMORY_A;
+    this.addrMode = AddressingMode.ABSOLUTE_INDEXED_INDIRECT;
+    this.mnemonic = 'JMP'
+}
+_jumpAbsoluteIndirect.prototype.run = function(args){
+    CPU.loadDataRegister(this.addrMode, this.size.getRealSize(), args);
+    CPU.pc.setValue(CPU.dataAddr);
+    var cycles = 6;
+    return cycles;
+}
+    
+    /**
+     * Jump Absolute Long
+     * 0x5C
+     */ 
+function _jumpAbsoluteLong() {
+    this.name = "Jump Absolute Long"
+    this.argCount = 0;
+    this.size = Size.MEMORY_A;
+    this.addrMode = AddressingMode.ABSOLUTE_LONG;
+    this.mnemonic = 'JMP'
+}
+_jumpAbsoluteLong.prototype.run = function(args){
+    CPU.loadDataRegister(this.addrMode, this.size.getRealSize(), args);
+    CPU.pc.setValue(CPU.dataAddr);
+    CPU.pbr.setValue(CPU.dataBank);
+    
+    var cycles = 4;
+    return cycles;
+}
+    
+    /**
+     * Jump Absolute Indirect Long
+     * 0xDC
+     */ 
+function _jumpAbsoluteIndirectLong() {
+    this.name = "Jump Absolute Indirect Long"
+    this.argCount = 0;
+    this.size = Size.MEMORY_A;
+    this.addrMode = AddressingMode.ABSOLUTE_INDIRECT_LONG;
+    this.mnemonic = 'JMP'
+}
+_jumpAbsoluteIndirectLong.prototype.run = function(args){
+    CPU.loadDataRegister(this.addrMode, this.size.getRealSize(), args);
+    CPU.pc.setValue(CPU.dataAddr);
+    CPU.pbr.setValue(CPU.dataBank);
+    
+    var cycles = 6;
+    return cycles;
+}
+    
+    /**
+     * Jump to Subroutine Absolute
+     * 0x20
+     */ 
+function _jumpSubAbsolute() {
+    this.name = "Jump to Subroutine Absolute"
+    this.argCount = 0;
+    this.size = Size.MEMORY_A;
+    this.addrMode = AddressingMode.ABSOLUTE;
+    this.mnemonic = 'JMP'
+}
+_jumpSubAbsolute.prototype.run = function(args){
+    CPU.loadDataRegister(this.addrMode, this.size.getRealSize(), args);
+    // Spec says to put the address of the last byte in this instruction
+    // PC points to the next instruction, so subtract one
+    CPU.stackPush(Size.SHORT, CPU.pc.getValue() - 1);
+    
+    CPU.pc.setValue(CPU.dataAddr);
+
+    var cycles = 6;
+    return cycles;
+}
+
+    /**
+     * Jump to Subroutine Absolute Indexed Indirect
+     * 0xFC
+     */ 
+function _jumpSubAbsoluteIndexedIndirect() {
+    this.name = "Jump to Subroutine Absolute Indexed Indirect"
+    this.argCount = 0;
+    this.size = Size.MEMORY_A;
+    this.addrMode = AddressingMode.ABSOLUTE_INDEXED_INDIRECT;
+    this.mnemonic = 'JSR'
+}
+_jumpSubAbsoluteIndexedIndirect.prototype.run = function(args){
+    CPU.loadDataRegister(this.addrMode, this.size.getRealSize(), args);
+    // Spec says to put the address of the last byte in this instruction
+    // PC points to the next instruction, so subtract one
+    CPU.stackPush(Size.SHORT, CPU.pc.getValue() - 1);
+    
+    CPU.pc.setValue(CPU.dataAddr);
+    CPU.pbr.setValue(CPU.dataBank);
+
+    var cycles = 8;
+    return cycles;
+}
+
+    /**
+     * Jump to Subroutine Long
+     * 0x22
+     */ 
+function _jumpSubLong() {
+    this.name = "Jump to Subroutine Long"
+    this.argCount = 0;
+    this.size = Size.MEMORY_A;
+    this.addrMode = AddressingMode.ABSOLUTE_LONG;
+    this.mnemonic = 'JSL'
+}
+_jumpSubLong.prototype.run = function(args){
+    CPU.loadDataRegister(this.addrMode, this.size.getRealSize(), args);
+    CPU.stackPush(Size.BYTE, CPU.pbr.getValue());
+    
+    // Spec says to put the address of the last byte in this instruction
+    // PC points to the next instruction, so subtract one
+    CPU.stackPush(Size.SHORT, CPU.pc.getValue() - 1);
+    
+    CPU.pc.setValue(CPU.dataAddr);
+    CPU.pbr.setValue(CPU.dataBank);
+    
+    var cycles = 8;
+    return cycles;
+}
+
+Jump = {
+    jumpAbsolute:                   new _jumpAbsolute(),
+    jumpAbsoluteIndirect:           new _jumpAbsoluteIndirect(),
+    jumpAbsoluteIndexedIndirect:    new _jumpAbsoluteIndexedIndirect(),
+    jumpAbsoluteLong:               new _jumpAbsoluteLong(),
+    jumpAbsoluteIndirectLong:       new _jumpAbsoluteIndirectLong(),
+    jumpSubAbsoluteIndexedIndirect: new _jumpSubAbsoluteIndexedIndirect(),
+    jumpSubLong:                    new _jumpSubLong()
+}
